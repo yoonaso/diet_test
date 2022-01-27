8babe049114210b4559dbda4062e50ac5a323a0f
yoonaso <67145615+yoonaso@users.noreply.github.com>
2022-01-27 16:47:38 +0900

    Add files via upload

---
+++ js/typed.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    var typed = new Typed('.typed', {
+        strings: ["나의 다이어트 유형은??"],
+        loop: false,
+        typeSpeed: 100,
+        showCursor: false,
+        cursorChar: "|",
+       
+      
+    });
+});
